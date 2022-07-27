727cc72d097c11bba9bc090e921e428ba58b51bd
Nasreen Banu <nasreenbanuak@gmail.com>
2022-07-27 12:06:04 +0530

    deleted springapp

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,11 +14,14 @@ import { LoginComponent } from './login/login.component';
     AppComponent,
     SignupComponent,
     LandingComponent,
-    LoginComponent
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
