72b319b5187e78c922812e04977779463b131f73
Nasreen Banu <nasreenbanuak@gmail.com>
2022-09-16 09:33:19 +0530

    Completed

--- springapp/src/main/java/com/example/demo/DemoApplication.java
+++ springapp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,37 @@
 package com.example.demo;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.model.ERole;
+import com.example.demo.model.Role;
+import com.example.demo.repository.RoleRepository;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Autowired
+	RoleRepository roleRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@PostConstruct
+	public void addRoles()
+	{
+		Role role1=new Role();
+		role1.setId(1);
+		role1.setName(ERole.ROLE_USER);
+		roleRepository.save(role1);
+		Role role2=new Role();
+		role2.setId(2);
+		role2.setName(ERole.ROLE_ADMIN);
+		roleRepository.save(role2);
+
+	}
 	
 
 }
